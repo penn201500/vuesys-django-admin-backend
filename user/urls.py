1aafe034a4e368de191efaae1839e5946efbb8e1
penn <penn201500@gmail.com>
2024-11-06 15:54:08 +0800

    add a test url for user views

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from user.views import TestView
+
+urlpatterns = [
+    path('test/', TestView.as_view(), name='test')
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+
+from user.models import SysUser
+
 
 # Create your views here.
+class TestView(View):
+    def get(self, request):
+        users = list(SysUser.objects.all().values())
+        res = {
+            'code': 200,
+            'data': users
+        }
+        return JsonResponse(res)
