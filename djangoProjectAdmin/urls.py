f05553990f7a6b16319fc3ebe96901e08e8480fd
penn <penn201500@gmail.com>
2024-11-06 13:09:38 +0800

    fix: use i18n_patterns wraps URL patterns to support multiple languages

--- djangoProjectAdmin/urls.py
+++ djangoProjectAdmin/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.i18n import urlpatterns
+from django.conf.urls.i18n import i18n_patterns
 # from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     # path("admin/", admin.site.urls),
     path("user/", include('user.urls')),
     path("role/", include('role.urls')),
     path("menu/", include('menu.urls')),
-]
+)
