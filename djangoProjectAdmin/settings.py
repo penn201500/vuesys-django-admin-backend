b6e8bfbf3d3c67cc8b54796be56e3aa03605ec5e
penn <penn201500@gmail.com>
2024-11-06 13:50:53 +0800

    use mysql as db

--- djangoProjectAdmin/settings.py
+++ djangoProjectAdmin/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "djangoProjectAdmin.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_admin',
+        'USER': 'djangoProjectAdmin',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
