c19108d240fd5bbe452ca3da6e9f15faa6962bae
penn <penn201500@gmail.com>
2024-11-10 04:12:26 +0800

    chore: comment debuggin print in user/authentication.py

--- user/authentication.py
+++ user/authentication.py
@@ -14,20 +14,20 @@ class CookieJWTAuthentication(JWTAuthentication):
         """
         Override the method to retrieve the token from the specified cookie.
         """
-        print("Attempting to retrieve accessToken from cookies")
+        # print("Attempting to retrieve accessToken from cookies")
         token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
-        print(f"Retrieved accessToken from cookies: {token}")
+        # print(f"Retrieved accessToken from cookies: {token}")
         return token
 
     def authenticate(self, request):
         access_token = self.get_raw_token(request)
 
-        print(f"Authenticating with access token: {access_token}")
+        # print(f"Authenticating with access token: {access_token}")
         if not access_token:
             return None
         try:
             validated_token = self.get_validated_token(access_token)
             return self.get_user(validated_token), validated_token
         except TokenError as e:
-            print(f"Token error: {e}")
+            # print(f"Token error: {e}")
             return None
