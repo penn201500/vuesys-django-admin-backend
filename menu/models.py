0d1a96ba9debb392e82cd24d9dde803445e9803e
penn <penn201500@gmail.com>
2024-12-12 20:50:46 +0800

    feat: add menu and role tables

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from rest_framework import serializers
+
+from role.models import SysRole
+
 
 # Create your models here.
+
+
+class SysMenu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True, verbose_name="Menu Name")
+    icon = models.CharField(max_length=100, null=True, verbose_name="Menu Icon")
+    parent_id = models.IntegerField(null=True, verbose_name="Parent Menu ID")
+    order_num = models.IntegerField(null=True, verbose_name="Order")
+    path = models.CharField(max_length=200, null=True, verbose_name="Router")
+    component = models.CharField(max_length=255, null=True, verbose_name="Path")
+    menu_type = models.CharField(
+        max_length=1, null=True, verbose_name="Menu Type（Catalog Menu Button）"
+    )
+    perms = models.CharField(max_length=100, null=True, verbose_name="Permission")
+    create_time = models.DateField(
+        null=True,
+        verbose_name="Created Time",
+    )
+    update_time = models.DateField(null=True, verbose_name="Updated Time")
+    remark = models.CharField(max_length=500, null=True, verbose_name="Comment")
+
+    def __lt__(self, other):
+        return self.order_num < other.order_num
+
+    class Meta:
+        db_table = "sys_menu"
+
+
+class SysMenuSerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    def get_children(self, obj):
+        print("111")
+        if hasattr(obj, "children"):
+            serializerMenuList: list[SysMenuSerializer2] = list()
+            for sysMenu in obj.children:
+                serializerMenuList.append(SysMenuSerializer2(sysMenu).data)
+            return serializerMenuList
+
+    class Meta:
+        model = SysMenu
+        fields = "__all__"
+
+
+class SysMenuSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = SysMenu
+        fields = "__all__"
+
+
+class SysRoleMenu(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.ForeignKey(SysRole, on_delete=models.PROTECT)
+    menu = models.ForeignKey(SysMenu, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "sys_role_menu"
+
+
+class SysRoleMenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SysRoleMenu
+        fields = "__all__"
--- role/models.py
+++ role/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from rest_framework import serializers
+
+from user.models import SysUser
+
 
 # Create your models here.
+
+
+class SysRole(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, null=True, verbose_name="Role Name")
+    code = models.CharField(
+        max_length=100, null=True, verbose_name="Role Permission Code"
+    )
+    create_time = models.DateField(
+        null=True,
+        verbose_name="Created Time",
+    )
+    update_time = models.DateField(null=True, verbose_name="Updated Time")
+    remark = models.CharField(max_length=500, null=True, verbose_name="Comment Time")
+
+    class Meta:
+        db_table = "sys_role"
+
+
+class SysRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SysRole
+        fields = "__all__"
+
+
+class SysUserRole(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.ForeignKey(SysRole, on_delete=models.PROTECT)
+    user = models.ForeignKey(SysUser, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "sys_user_role"
+
+
+class SysUserRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SysUserRole
+        fields = "__amll__"
