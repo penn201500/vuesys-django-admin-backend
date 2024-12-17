a945349bdcd6534643218242f915088ecdc26050
penn <penn201500@gmail.com>
2024-12-17 17:38:38 +0800

    opt: remove redundant login_date field in user model

--- user/models.py
+++ user/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class SysUser(AbstractUser):
-    avatar = models.CharField(max_length=255, null=True, verbose_name='Avatar')
-    email = models.CharField(max_length=100, null=True, verbose_name='Email')
-    phone = models.CharField(max_length=11, null=True, verbose_name='Phone')
-    login_date = models.DateTimeField(null=True, verbose_name='Login Date')
-    status = models.IntegerField(null=True, verbose_name='Status')
-    create_time = models.DateTimeField(null=True, verbose_name='Create Time')
-    update_time = models.DateTimeField(null=True, verbose_name='Update Time')
-    comment = models.CharField(max_length=500, null=True, verbose_name='Comment')
+    avatar = models.CharField(max_length=255, null=True, verbose_name="Avatar")
+    email = models.CharField(max_length=100, null=True, verbose_name="Email")
+    phone = models.CharField(max_length=11, null=True, verbose_name="Phone")
+    status = models.IntegerField(null=True, verbose_name="Status")
+    create_time = models.DateTimeField(null=True, verbose_name="Create Time")
+    update_time = models.DateTimeField(null=True, verbose_name="Update Time")
+    comment = models.CharField(max_length=500, null=True, verbose_name="Comment")
 
     class Meta:
-        db_table = 'sys_user'
+        db_table = "sys_user"
