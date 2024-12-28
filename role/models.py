f92f10a23f60317ea694db35944215c949a38f61
penn <penn201500@gmail.com>
2024-12-28 17:33:04 +0800

    feat: add status,is_system,deleted_at fields in role model

--- role/models.py
+++ role/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from rest_framework import serializers
+from django.utils import timezone
 
 from user.models import SysUser
 
@@ -13,6 +14,15 @@ class SysRole(models.Model):
     code = models.CharField(
         max_length=100, null=True, verbose_name="Role Permission Code"
     )
+    status = models.IntegerField(
+        default=1, verbose_name="Status"
+    )  # 1: active, 0: inactive
+    is_system = models.BooleanField(
+        default=False, verbose_name="Is System Role"
+    )  # For admin/common roles
+    deleted_at = models.DateTimeField(
+        null=True, blank=True, verbose_name="Deletion Date"
+    )  # For soft delete
     create_time = models.DateField(
         null=True,
         verbose_name="Created Time",
@@ -22,6 +32,15 @@ class SysRole(models.Model):
 
     class Meta:
         db_table = "sys_role"
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.status = 0
+        self.save()
+
+    @property
+    def is_active(self):
+        return self.status == 1 and not self.deleted_at
 
 
 class SysRoleSerializer(serializers.ModelSerializer):
